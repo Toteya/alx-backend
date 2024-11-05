cf314add445b230f8ba1f827cc749d707a4bf7c8
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-05 16:35:42 +0200

    try - set babel default in Config class

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -12,6 +12,8 @@ class Config:
     """ Babel configurations class
     """
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
