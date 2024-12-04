f1cf32d3169771f226e460f89fd82bfd7921cb97
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-04 14:53:06 +0200

    refactored to use node-redis version 2.8.0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -9,20 +9,23 @@ const client = createClient();
 client.on('error', error => {
   console.log(`Redis client not connected to the server: ${error}`)
 });
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+})
 
-await client.connect()
-  .then(() => {
-    console.log('Redis client connected to the server');
-  })
-  .catch((error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
-  });
+// await client.connect()
+//   .then(() => {
+//     console.log('Redis client connected to the server');
+//   })
+//   .catch((error) => {
+//     console.log(`Redis client not connected to the server: ${error}`);
+//   });
 
-async function startRedisServer() {
-  const execP = promisify(exec);
-  await execP('redis-6.0.10/src/redis-server')
-    .then(() => {})
-    .catch((error) => {
-      console.log(error.message);
-    });
-}
+// async function startRedisServer() {
+//   const execP = promisify(exec);
+//   await execP('redis-6.0.10/src/redis-server')
+//     .then(() => {})
+//     .catch((error) => {
+//       console.log(error.message);
+//     });
+// }
