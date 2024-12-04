1a71a06d6f811e4a229e661231463178da3e7f8f
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-04 14:54:16 +0200

    removed unnecessary code

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,8 +1,4 @@
 import { createClient } from 'redis';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
-// startRedisServer();
 
 const client = createClient();
 
@@ -12,20 +8,3 @@ client.on('error', error => {
 client.on('ready', () => {
   console.log('Redis client connected to the server');
 })
-
-// await client.connect()
-//   .then(() => {
-//     console.log('Redis client connected to the server');
-//   })
-//   .catch((error) => {
-//     console.log(`Redis client not connected to the server: ${error}`);
-//   });
-
-// async function startRedisServer() {
-//   const execP = promisify(exec);
-//   await execP('redis-6.0.10/src/redis-server')
-//     .then(() => {})
-//     .catch((error) => {
-//       console.log(error.message);
-//     });
-// }
