128503cd62e269ff4d40177dd143b41ad1b8a19a
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-07 10:48:50 +0200

    refactored: better code

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -9,12 +9,14 @@ client.on('ready', () => {
   console.log('Redis client connected to the server');
 })
 
-function setNewSchool(schoolName, value) {
+function setNewSchool(schoolName, value, callback) {
   client.set(schoolName, value, redis.print);
 }
 
 function displaySchoolValue(schoolName) {
-  client.get(schoolName, redis.print);
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
 }
 
 displaySchoolValue('Holberton');
